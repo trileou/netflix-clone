09bb5bd60a6ac39a2c5b155a4af4e0d3616eccd8
tri.le <hnguyen@kms-solutions.asia>
2021-02-26 18:40:16 +0700

    commit

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import { Inner, Container, Title, SubTitle, Pane, Image, Item } from './styles/jumbotron';
+import React from "react";
+import {
+  Inner,
+  Container,
+  Title,
+  SubTitle,
+  Pane,
+  Image,
+  Item,
+} from "./styles/jumbotron";
 
-export default function Jumbotron({ children, direction = 'row', ...restProps }) {
+export default function Jumbotron({
+  children,
+  direction = "row",
+  ...restProps
+}) {
   return (
-    <Item direction={direction}>
+    <Item {...restProps} direction={direction}>
       <Inner>{children}</Inner>
     </Item>
   );
