b08da3e1c3b37d329db4dbcd6b954fbd3935a676
tri.le <hnguyen@kms-solutions.asia>
2021-02-26 16:32:03 +0700

    update app.js format

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import React from "react";
-import jumboData from "./fixtures/jumbo.json";
-import Jumbotron from "./components/jumbotron";
+import React from 'react';
+import jumboData from './fixtures/jumbo.json';
+import Jumbotron from './components/jumbotron';
 
 export default function App() {
   return (
