b47ab6990546823a3d357044442115ff906dbf84
tri.le <hnguyen@kms-solutions.asia>
2021-03-01 10:19:03 +0700

    Update footer

---
+++ src/containers/footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Footer } from "../components";
+
+export function FooterContainer() {
+  return (
+    <Footer>
+      <Footer.Title>Question? Contact Us</Footer.Title>
+      <Footer.Break>
+        <Footer.Row>
+          <Footer.Column>
+            <Footer.Link href="#">FAQ</Footer.Link>
+            <Footer.Link href="#">Investor Relation</Footer.Link>
+            <Footer.Link href="#">Ways to Watch</Footer.Link>
+            <Footer.Link href="#">Coporate Information</Footer.Link>
+            <Footer.Link href="#">Netflix Originals</Footer.Link>
+          </Footer.Column>
+        </Footer.Row>
+      </Footer.Break>
+    </Footer>
+  );
+}
